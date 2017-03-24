2a37d80ce91d8cd2c95f775702e950d53ea1b363
stephen.wille <stephen.wille@gmail.com>
2017-03-23 19:16:58 -0700

    books resource endpoint

---
+++ src/main/java/com/pluralsight/Book.java
@@ -0,0 +1,56 @@
+package com.pluralsight;
+
+import java.util.Date;
+
+public class Book  {
+    private String author;
+    private Date published;
+    private String title;
+    private String isbn;
+    private String id;
+
+
+    public Book(){}
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Date getPublished() {
+        return published;
+    }
+
+    public void setPublished(Date published) {
+        this.published = published;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+}
