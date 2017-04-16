32f2864dbe57e8121d178a171d059c4829685b9e
stephen.wille <stephen.wille@gmail.com>
2017-04-16 10:57:57 -0700

    async w/ guava and moxy

--- src/main/java/com/pluralsight/BookDao.java
+++ src/main/java/com/pluralsight/BookDao.java
@@ -1,8 +1,9 @@
 package com.pluralsight;
 
-import jersey.repackaged.com.google.common.util.concurrent.ListenableFuture;
-import jersey.repackaged.com.google.common.util.concurrent.ListeningExecutorService;
-import jersey.repackaged.com.google.common.util.concurrent.MoreExecutors;
+
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 
 import java.util.Collection;
 import java.util.Map;
@@ -26,8 +27,29 @@ public class BookDao {
         return(books.values());
     }
 
+    ListenableFuture<Collection<Book>> getBooksAsync() {
+        ListenableFuture<Collection<Book>> future = service.submit(new Callable<Collection<Book>>() {
+            @Override
+            public Collection<Book> call() throws Exception {
+                return getBooks();
+            }
+        });
+        return future;
+    }
+
+
+
     Book getBook(String id) {
         return(books.get(id));
+    }
+    ListenableFuture<Book> getBookAsync(final String id) {
+        ListenableFuture<Book> future = service.submit(new Callable<Book>() {
+            @Override
+            public Book call() throws Exception {
+                return getBook(id);
+            }
+        });
+        return future;
     }
 
     Book addBook(Book book) {
@@ -35,7 +57,6 @@ public class BookDao {
         books.put(book.getId(), book);
         return(book);
     }
-
     ListenableFuture<Book> addBookAsync(final Book book) {
         ListenableFuture<Book> future =
                 service.submit(new Callable<Book>() {
